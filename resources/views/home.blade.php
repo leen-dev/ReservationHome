6c99fb7b99c37eb77cab99bfca1b4e2cf1fafed5
leen-dev <ekouekatygracia@gmail.com>
2024-04-11 11:16:42 +0000

    code complet

---
+++ resources/views/home.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('resources/css/home.css') }}">
+    <title>Document</title>
+</head>
+<body>
+
+    <section class="salles" id="salles">
+        <div class="contenu">
+            <div class="titre">
+                <h2 class="titre-texte">
+                    Salles <span>disponibles</span>
+                </h2>
+                <p>
+                    Commencez par reserver chez <a href="#" class="logo"> <span>R</span>eservation<span>H</span>ome </a>
+                </p>
+            </div>
+        </div>
+        <div class="contenu">
+            <div class="box">
+                <div class="imbox">
+                    <img src="images/salades.png" alt="sallebigmate" id="i1">
+                </div>
+                <div class="text">
+                    <h3>
+                        Salles BigMate
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="Salle Pavllion" id="i2">
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle Pavllion
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="Salle AuMonsieur"  >
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle AuMonsieur
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="Salle DangBit">
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle DangBit
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="Salle VueDieu"  >
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle VueDieu
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="Salle deJeanPierre"  >
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle deJeanPierre
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="lemollet" >
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle LeMollet
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="imbox">
+                    <img src="" alt="Salle Demerite"  >
+                </div>
+                <div class="text">
+                    <h3>
+                        Salle Demerite
+                    </h3>
+                    <p>
+                        <ul class="caracteristiques">
+                            <li>
+                                15 Places
+                            </li>
+                            <li>
+                                Table centrale
+                            </li>
+                            <li>
+                                Projecteurs
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="titre">
+            <a href="#" class="btn1">
+                Voir Plus
+            </a>
+        </div>
+    </section>
+
+</body>
+</html>
